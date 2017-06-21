d022c2ba4bfa1929cb7561332a8a14abea9b81e3
Dustin Walker <thewalkman1234@gmail.com>
2017-06-21 10:31:03 -0500

    reverse and alphabetical function built

--- string.js
+++ string.js
@@ -6,31 +6,50 @@
 // When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
 // The output of each of the functions should immediately appear as well.
 
-var string = [];
-
-var input = document.getElementById("input");
-var button = document.getElementById("button");
-
-button.addEventListener("click", function() {
-	string = input.value;
-})
-
-console.log(string)
-
-
-function reversal() {
-
-}
-
-function alphabits() {
-
-}
-
-function palindrome() {
-
-}
 
 var testString = "";
-reversal(testString);
-alphabits(testString);
+
+var button = document.getElementById("button");
+button.addEventListener("click", function() {
+	testString = document.getElementById("input").value;
+	reversal(testString);
+	alphabits(testString);
+	
+});
+
+
+
+function reversal(string) {
+	var reverseString = testString.split('').reverse().join('');
+	let output = document.getElementById("output");
+	output.innerHTML = reverseString;
+	// console.log(reverseString);
+};
+
+function alphabits(string) {
+	var alphabetical = string.split('').sort().join('');
+	// console.log(alphabetical);
+	let output = document.getElementById("output");
+	output.innerHTML = alphabetical;
+	
+}
+
+function palindrome(string) {
+	var stringArr = string.split('');
+	console.log(stringArr);
+	var reverseString2 = string.split('').reverse();
+	console.log(reverseString2);
+	if (stringArr === reverseString2) {
+		console.log("palindrome")
+	};
+
+};
+
+
+
+
+
+
 palindrome(testString);
+
+"MIBO".split("").sort().join("");  // "BIMO"
